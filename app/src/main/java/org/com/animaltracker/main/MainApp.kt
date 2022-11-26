672d5d7373aa151d36618c4348dd92443edd6561
tommyd450 <tommyd811@gmail.com>
2022-11-26 15:58:23 +0000

    JSON Persistence of Animal Model objects including updating and deletion

--- app/src/main/java/org/com/animaltracker/main/MainApp.kt
+++ app/src/main/java/org/com/animaltracker/main/MainApp.kt
@@ -1,18 +1,20 @@
 package org.com.animaltracker.main
 
 import android.app.Application
+import org.com.animaltracker.model.AnimalJSONStore
 import org.com.animaltracker.model.AnimalMemStore
+import org.com.animaltracker.model.AnimalStore
 import timber.log.Timber
 
 class MainApp : Application() {
 
-    lateinit var animals : AnimalMemStore
+    lateinit var animals : AnimalStore
 
     override fun onCreate() {
         print("here")
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        animals = AnimalMemStore()
+        animals = AnimalJSONStore(applicationContext)
         Timber.i("Placemark started")
     }
 
