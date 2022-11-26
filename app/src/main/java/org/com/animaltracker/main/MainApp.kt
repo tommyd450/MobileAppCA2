b840734552df973268a890f8d19140ecef80bc5c
tommyd450 <tommyd811@gmail.com>
2022-11-26 15:38:48 +0000

    FileHelpers

--- app/src/main/java/org/com/animaltracker/main/MainApp.kt
+++ app/src/main/java/org/com/animaltracker/main/MainApp.kt
@@ -6,12 +6,13 @@ import timber.log.Timber
 
 class MainApp : Application() {
 
-    val animals = AnimalMemStore()
+    lateinit var animals : AnimalMemStore
 
     override fun onCreate() {
         print("here")
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        animals = AnimalMemStore()
         Timber.i("Placemark started")
     }
 
